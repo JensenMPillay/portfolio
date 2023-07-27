39b4babdccf8aae9d3835308ecb97f9686f1b3ba
JensenMPillay <jensen_pillay@hotmail.fr>
2023-07-27 19:05:48 +0200

    Update Index Title & Intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,9 +34,9 @@ export default function Home() {
             <div className="flex w-1/2 flex-col items-center self-center lg:w-full lg:text-center">
               <AnimatedText
                 text="Turning Vision Into Reality With Code And Design."
-                className="xl:!text-5xl lg:text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
+                className="xl:!text-6xl lg:text-center lg:!text-5xl md:!text-4xl sm:!text-3xl"
               />
-              <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
+              <p className="my-4 text-lg font-medium lg:text-base md:text-sm sm:text-xs">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
