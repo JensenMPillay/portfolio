1d9962aeb599b972ee5230046d920281b87e4b9f
JensenMPillay <jensen_pillay@hotmail.fr>
2024-01-25 15:15:46 +0100

    [Style] Change SocialIcons Hover Behavior

--- src/components/Navbar/SocialIcons.js
+++ src/components/Navbar/SocialIcons.js
@@ -1,13 +1,11 @@
-import React from "react";
-import { GithubIcon, LinkedInIcon, MailIcon } from "../Icons/Icons";
 import { motion } from "framer-motion";
+import { GithubIcon, LinkedInIcon, MailIcon } from "../Icons/Icons";
 
 const SocialIconContainer = ({ href, index, name, children }) => {
   const icon = {
     initial: {
       scale: 0,
       opacity: 0,
-      rotate: 0,
     },
     animate: (customValue) => ({
       scale: [0, 1, 1.5, 3, 1.5, 1],
@@ -19,28 +17,20 @@ const SocialIconContainer = ({ href, index, name, children }) => {
         delay: customValue * 0.2 + 1,
       },
     }),
-    hover: {
-      rotate: 180,
-      transition: {
-        type: "inertia",
-        velocity: 450,
-      },
-      duration:0.5,
-    },
   };
   return (
     <motion.a
       href={href}
       target="_blank"
-      className="w-12 relative group"
+      className="group relative w-12"
       variants={icon}
       initial="initial"
       animate="animate"
-      whileHover="hover"
       custom={index}
     >
-      <span
-       className="absolute text-dark transition-all duration-300 group-hover:delay-1000 delay-0 opacity-0 group-hover:opacity-100 dark:text-light top-1/2 left-full -translate-y-1/2">{name}</span>
+      <span className="absolute left-full top-1/2 -translate-y-1/2 text-dark opacity-0 transition-all delay-0 duration-300 group-hover:opacity-100 group-hover:duration-300 dark:text-light">
+        {name}
+      </span>
       {children}
     </motion.a>
   );
@@ -49,22 +39,26 @@ const SocialIconContainer = ({ href, index, name, children }) => {
 const SocialIcons = () => {
   return (
     <div className="fixed bottom-0 left-8 flex flex-col items-center justify-center text-dark dark:text-light lg:left-4 md:left-1">
-        <SocialIconContainer
-        name="Github"
-          href="https://www.github.com/JensenMPillay"
-          index="3"
-        >
-          <GithubIcon />
-        </SocialIconContainer>
       <SocialIconContainer
-      name="LinkedIn"
+        name="Github"
+        href="https://www.github.com/JensenMPillay"
+        index="3"
+      >
+        <GithubIcon className="transition-all duration-300 group-hover:scale-125" />
+      </SocialIconContainer>
+      <SocialIconContainer
+        name="LinkedIn"
         href="https://www.linkedin.com/in/jensenpillay/"
         index="2"
       >
-        <LinkedInIcon />
+        <LinkedInIcon className="transition-all duration-300 group-hover:scale-125" />
       </SocialIconContainer>
-      <SocialIconContainer name="Send&nbsp;Email" href="mailto:jmooroongapillay@gmail.com" index="1">
-        <MailIcon />
+      <SocialIconContainer
+        name="Send&nbsp;Email"
+        href="mailto:jmooroongapillay@gmail.com"
+        index="1"
+      >
+        <MailIcon className="transition-all duration-300 group-hover:scale-125" />
       </SocialIconContainer>
       <motion.span
         className="h-24 w-0.5 bg-dark dark:bg-light"
