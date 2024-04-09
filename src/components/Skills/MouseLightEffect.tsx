c56f2c4cdb04091633e9e29d77486045abab5081
JensenMPillay <jensen_pillay@hotmail.fr>
2024-04-09 15:41:10 +0200

    [Style] Change MouseLight Color

--- src/components/Skills/MouseLightEffect.tsx
+++ src/components/Skills/MouseLightEffect.tsx
@@ -10,7 +10,7 @@ const MouseLightEffect = ({ mouseX, mouseY }: Props) => {
     <motion.div
       className="pointer-events-none absolute -inset-px rounded-lg opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100"
       style={{
-        background: useMotionTemplate`radial-gradient(100px circle at ${mouseX}px ${mouseY}px, rgba(1,51,40,1), transparent 80%)`,
+        background: useMotionTemplate`radial-gradient(100px circle at ${mouseX}px ${mouseY}px, rgba(255,255,255,0.2), transparent 50%)`,
       }}
     ></motion.div>
   );
